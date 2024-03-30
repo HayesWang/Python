da955e5b2de129d1cd82e1da26b6a3877d5c6e35
王天傲 <wang-johnson@outlook.com>
2024-03-30 22:43:16 +0800

    PYCHARM

--- Week5/10.py
+++ Week5/10.py
@@ -2,10 +2,11 @@
 # 否则输出a*a+b*b。例如，输入25 25 ，它们的平方和为625+625=1250，百位及以上数为12.
 # 输入：两个整数，中间用空格隔开
 # 输出：一个整数。
-a,b=int(input().split(' '))
-sum=a*a+b*b
+a, b = input().split(' ')
+a = int(a)
+b = int(b)
+sum = int(a * a + b * b)
 if sum >= 100:
-    print(sum/100)
+    print(int(sum / 100))
 else:
     print(sum)
-    
