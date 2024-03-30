09c72fb3688c69e77448234644de70563123ef8d
王天傲 <wang-johnson@outlook.com>
2024-03-30 16:12:20 +0800

    3.30晚上

--- Week5/7.py
+++ Week5/7.py
@@ -2,12 +2,13 @@
 # 将相加的结果以及按四舍五入方法转换成整数后的结果显示输出。
 # 输入：3个小数，空格分隔；
 # 输出：和，四舍五入后的整数，1行显示，空格分隔。
-a=float(input())
-b=float(input())
-c=float(input())
-sum=a+b+c
-print(sum,end=' ')
-if sum%1 >= 0.5:
+a=input()
+sum=0
+for i in a.split(" "):
+    sum+=float(i)
+sum=int(sum*100000)/100000
+print(sum,end=" ")
+if sum%1 >=0.5:
     print(int(sum)+1)
 else:
     print(int(sum))
