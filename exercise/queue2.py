a99eb0346715213f1fe1d15eefb16e086f1865ac
王天傲 <wang-johnson@outlook.com>
2024-04-15 14:09:23 +0800

    循环队列

--- exercise/queue2.py
+++ exercise/queue2.py
@@ -1,23 +1,42 @@
+#实现循环队列基本操作并测试，需实现以下方法：
+#__init__(),  is_empty(),   is_full()
+#enqueue() ,  dequeue(),   display() ,  size()  
 class Queue:
-    def __init__(self,maxSize=100):
+    def __init__(self,maxSize=101):
         self.maxSize=maxSize
         self.items=[0]*maxSize
         self.front=0
         self.rear=0
     def enqueue(self,item):
-        if not self.is_leak:
+        if not self.is_full():
             self.rear=(self.rear+1)%self.maxSize
             self.items[self.rear]=item
+            print("enq",end=" ")
         else:
-            print("Leak")
+            print("Full")
     def dequeue(self):
-        if not self.is_empty:
+        if not self.is_empty():
             self.front=(self.front+1)%self.maxSize
             return self.items[self.front]
-    def is_leak(self):
-        if (self.rear+1)%self.maxSize==self.front:
-            return True
-        else:
-            return False
+    def is_full(self):
+        return (self.rear + 1)%self.maxSize == self.front
     def is_empty(self):
         return self.items==[]
+
+q = Queue()
+#0,50 正常输入输出
+for i in range(0,50):
+    q.enqueue(i)
+print("")
+for i in range(0,50):
+    print(q.dequeue(),end=" ")
+print("")
+#再输入100个，测试循环性
+for i in range(0,100):
+    q.enqueue(i)
+print("")
+for i in range(0,100):
+    print(q.dequeue(),end=" ")
+#输入125个，测试溢出
+for i in range(0,120):
+    q.enqueue(i)
