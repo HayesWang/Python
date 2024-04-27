5fb98a923b9ca07647faa528735330b91c6d1ab5
王天傲 <wang-johnson@outlook.com>
2024-04-27 14:43:42 +0800

    PYCHARM

--- Week7/7.1.py
+++ Week7/7.1.py
@@ -2,6 +2,19 @@
 # 样例：
 #    abc  de
 #    abcde
-a=input().split(" ")
+a = input()
+firstletter = 0
+lastletter = 0
 for i in range(0,len(a)):
+    if a[i] != " " and firstletter == 0:
+        firstletter = i
+    if a[i] != " ":
+        lastletter = i
+
+for i in range(0,firstletter):
     print(a[i],end="")
+for i in range(firstletter,lastletter+1):
+    if a[i] != " ":
+        print(a[i],end="")
+for i in range(lastletter+1,len(a)):
+    print(a[i])
